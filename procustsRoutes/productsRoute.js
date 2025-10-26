7abb195411f7a7ff01b55a8515484331c23b489e
yashif khan <yashifyashifkhan@gmail.com>
2025-10-26 21:02:30 +0530

    resolve errors in testing

--- procustsRoutes/productsRoute.js
+++ procustsRoutes/productsRoute.js
@@ -1,49 +1,3 @@
-// import express from "express";
-// const router=express.Router()
-// const productRouter=router
-// import path from "path"
-// import multer from "multer"
-// import fs from "fs"
-
-
-// const UPLOAD_DIR = process.env.UPLOAD_DIR || 'uploads';
-
-// // ensure upload dir exists
-// if (!fs.existsSync(UPLOAD_DIR)) {
-//   fs.mkdirSync(UPLOAD_DIR, { recursive: true });
-// }
-
-// const storage = multer.diskStorage({
-//     destination:(req,file,cb)=>{
-//         cb(null,UPLOAD_DIR)
-//     },
-//     filename:(req,file ,cb)=>{
-//         const ext=path.extname(file.originalname);
-//         const name=Date.now()+ '-'+Math.round(Math.random() * 1e9)+ext;
-//         cb(null,name)
-//     }
-// })
-
-// const fileFilter =(req,file,cb)=>{
-//     if(file.mimtype.startsWith('image/')) cb(null,true);
-//     else cb(new Error('Only image files are allowed!'),false)
-// }
-// const uploads =multer({
-//     storage,fileFilter,
-//     limits:{fileSize:2*1024*1024}
-// })
-
-
-// app.use('/uploads', express.static(path.join(__dirname, UPLOAD_DIR)));
-// import { addProduct, getProducts } from "../productsController/productsController.js";
-
-// productRouter.route("/addProduct",uploads.single('image')).post(addProduct)
-// productRouter.route("/getProducts/:id").get(getProducts)
-
-// export {productRouter}
-
-
-
 
 import express from "express";
 import path from "path";
@@ -53,10 +7,7 @@ import { addOfferProducts, addProduct, getAllProducts, getOffersProducts, getPro
 
 const productRouter = express.Router();
 
-// --- Upload config ---
 const UPLOAD_DIR = process.env.UPLOAD_DIR || 'uploads';
-
-// ensure upload dir exists
 if (!fs.existsSync(UPLOAD_DIR)) {
   fs.mkdirSync(UPLOAD_DIR, { recursive: true });
 }
